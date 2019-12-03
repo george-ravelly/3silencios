b36c4f4b819747a7ff62cf5cc04d8c9430c4d2f1
George Ravelly <george.ravelly1@gmail.com>
2019-12-03 19:39:38 -0300

    criação de rotas

---
+++ src/config.js
@@ -0,0 +1,9 @@
+/*const config = {
+    apiKey: "AIzaSyDnatWbEbaAugOYJXO1IcCgGFmq541wpco",
+    authDomain: "silenciosaplicacao.firebaseapp.com",
+    databaseURL: "https://silenciosaplicacao.firebaseio.com",
+    storageBucket: "silenciosaplicacao.appspot.com"      
+};
+// Initialize Firebase
+config.initializeApp(firebaseConfig);
+config.analytics();*/
