de8c9d63aa4448d12898b9dae0aeab26f004e082
George Ravelly da Silva Ribeiro <49557536+George1050@users.noreply.github.com>
2019-12-05 08:32:42 -0300

    Update sobre.js

--- src/Component/sobre.js
+++ src/Component/sobre.js
@@ -4,14 +4,16 @@ const sobre = () => (
         <h1 className="display-4">Olá, Guerreiro!</h1>
         <p className="lead">Loja feita para o fã de Fantasia!!!</p>
         <hr className="my-4" />
-        <p className="text-justify">
-            Sunt nisi velit et culpa aliquip labore non ut exercitation est exercitation eu. <br/>
-            Commodo sint aliqua sit in laboris do laboris. Et enim labore voluptate elit<br/> 
-            exercitation aliquip dolore ad enim ad. Laborum officia mollit Lorem voluptate<br/> 
-            mollit. Est tempor culpa Lorem cupidatat proident labore quis elit ut magna. <br/>
-            Consectetur ad elit exercitation velit proident dolor incididunt amet dolore esse<br/> 
-            ipsum voluptate. Est enim ea Lorem aliquip minim veniam.<br/>
-        </p>
+        <section>
+            <p className="text-justify">
+                Sunt nisi velit et culpa aliquip labore non ut exercitation est exercitation eu. <br/>
+                Commodo sint aliqua sit in laboris do laboris. Et enim labore voluptate elit<br/> 
+                exercitation aliquip dolore ad enim ad. Laborum officia mollit Lorem voluptate<br/> 
+                mollit. Est tempor culpa Lorem cupidatat proident labore quis elit ut magna. <br/>
+                Consectetur ad elit exercitation velit proident dolor incididunt amet dolore esse<br/> 
+                ipsum voluptate. Est enim ea Lorem aliquip minim veniam.<br/>
+            </p>
+        </section>
     </div>
 );
 export default sobre;
